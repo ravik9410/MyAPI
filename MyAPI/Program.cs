3ceabba8b302a4b8377eda5802293eb61c8acaee
Ravi Kumar5 <ravik5@chetu.com>
2024-05-27 21:41:10 +0530

    changes the git

--- MyAPI/Program.cs
+++ MyAPI/Program.cs
@@ -10,47 +10,14 @@ builder.Services.AddControllers();
 var dbHost = Environment.GetEnvironmentVariable("DB_HOST");
 var dbName = Environment.GetEnvironmentVariable("DB_NAME");
 var dbPassword = Environment.GetEnvironmentVariable("DB_SA_PASSWORD");
-var connectionString = $"Data Source={dbHost};Initial Catalog={dbName}; Persist Security Info=True; User ID=sa; Password={dbPassword}; Pooling=False;Encrypt=False;";
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-{
-    options.UseSqlServer(connectionString);
-});
+var connectionString = $"Data Source={dbHost};Initial Catalog={dbName};User ID=sa;Password={dbPassword}";
+builder.Services.AddDbContext<ApplicationDbContext>(opt => opt.UseSqlServer(connectionString));
+/* ===================================== */
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-/*//builder.Services.AddHsts(options =>
-//{
-//    options.Preload = true;
-//    options.IncludeSubDomains = true;
-//    options.MaxAge = TimeSpan.FromDays(60);
-//    options.ExcludedHosts.Add("example.com");
-//    options.ExcludedHosts.Add("www.example.com");
-//});
-//builder.Services.AddHttpsRedirection(options =>
-//{
-//    options.RedirectStatusCode = Status307TemporaryRedirect;
-//    options.HttpsPort = 7016;
-//});
-//if (!builder.Environment.IsDevelopment())
-//{
-//    builder.Services.AddHttpsRedirection(options =>
-//    {
-//        options.RedirectStatusCode = Status308PermanentRedirect;
-//        options.HttpsPort = 443;
-//    });
-//}*/
-
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-/*
-app.UseSwagger();
-app.UseSwaggerUI();*/
-/*if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Error");
-    app.UseHsts();
-}*/
 app.UseSwagger();
 //app.UseSwaggerUI();
 app.UseSwaggerUI(c =>
@@ -59,6 +26,11 @@ app.UseSwaggerUI(c =>
     // c.InjectStylesheet("/swagger/custom.css");
     c.RoutePrefix = String.Empty;
 });
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+}
+
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseRouting();
